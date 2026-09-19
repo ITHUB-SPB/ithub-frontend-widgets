59fb6b3a985bc48334a06de66925b5bf5a56d477
Трофимов Алексей <a.trofimov@domain.local>
2026-09-19 16:50:10 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,3 @@
-import { useState } from 'react'
-
-import Counter from './components/counter'
-import './App.css'
-
 export default function App() {
-  const [clicks, updateClicks] = useState<number>(0)
 
-  const handleClick = () => updateClicks(state => state + 1)
-
-  return (
-    <>
-      <p>Кликов: {clicks}</p>
-      <section id="center">
-        <Counter min={null} max={10} updateClicks={handleClick} />
-        <Counter min={3} max={null} updateClicks={handleClick} />
-      </section>
-    </>
-  )
 }
